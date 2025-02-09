46fcb15f4720c3d1a60ca7d56131b0eb903e1c98
BrandonGriffith <BrandonGriffith92@gmail.com>
2025-02-08 23:20:19 -0500

    feat(data_entry): add stub functions for user input prompts

---
+++ data_entry.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+def get_date(prompt, allow_default=False):
+    raise NotImplementedError
+
+def get_amount(prompt):
+    raise NotImplementedError
+
+def get_category(prompt):
+    raise NotImplementedError
+
+def get_description(prompt):
+    raise NotImplementedError
